e19413d11ac4ff80b954d2a59269b4331db683e2
kaisadilla <58343029+kaisadilla@users.noreply.github.com>
2025-08-04 02:27:28 +0200

    Lua API documentation.

--- scripts/characters/api.d.lua
+++ scripts/characters/api.d.lua
@@ -1,3 +1,13 @@
+-- #region Global functions
+--- Waits for the amount of time given before executing the next instruction.
+---@param ms number The amount of time, in milliseconds, to wait.
+function wait(ms) end
+
+--- Opens a message textbox with the text provided.
+---@param text string The text to display.
+function message(text) end
+-- #endregion Global functions
+
 ---@class Character
 local Character = {}
 
@@ -21,6 +31,26 @@ function Character.move_left(steps) end
 ---@overload fun(steps: integer): void
 function Character.move_right(steps) end
 
+--- Makes the character look upwards.
+function Character.look_up() end
+
+--- Makes the character look downwards.
+function Character.look_down() end
+
+--- Makes the character look to the left.
+function Character.look_left() end
+
+--- Makes the character look to the right.
+function Character.look_right() end
+
+--- Makes the character look towards the player.
+function Character.look_towards_player() end
+
+--- Makes the character jump in place n times (1 if no amount is provided),
+---@overload fun(): void
+---@overload fun(times: integer): void
+function Character.jump(times) end
+
 --- Makes the character start or stop ignoring other characters while moving.
 ---@overload fun(): void 
 ---@overload fun(val: boolean): void
@@ -29,7 +59,5 @@ function Character.ignore_characters(val) end
 
 ---@type Character -- The character that owns this interaction.
 target = {}
-
---- Opens a message textbox with the text provided.
----@param text string The text to display.
-function message(text) end
+---@type Character -- The player.
+player = {}
--- scripts/characters/test-map-1/winona.lua
+++ scripts/characters/test-map-1/winona.lua
@@ -1,5 +1,21 @@
+target:look_left()
+wait(750)
+target:look_right()
+wait(750)
+target:look_towards_player()
+target:jump()
+message(
+    "Wow, we can speak, but there's a pandemic so you'll have to respect our distance."
+)
+player:jump()
 target:move_down(2)
 target:move_left()
+target:look_towards_player()
+player:jump()
+player:jump()
+message(
+    "Get ready, I'm about to tell you a very long paragraph."
+)
 message(
     "When I was a kid, all of this was black empty tiles. " ..
     "Now there's trees and stuff. There's also textboxes, " ..
